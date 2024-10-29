3144d1aa40cd3ac802eae05e45963ace19dff860
Straton Andre Antonio <andre.straton@student.unitbv.ro>
2024-10-29 13:01:52 +0200

    Add logging utilities for server events

---
+++ server/src/core/DatabaseManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Asula on 10/29/2024.
+//
+
+#include "DatabaseManager.hpp"
---
+++ server/src/core/DatabaseManager.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Asula on 10/29/2024.
+//
+
+#ifndef DATABASEMANAGER_H
+#define DATABASEMANAGER_H
+
+
+
+class DatabaseManager {
+
+};
+
+
+
+#endif //DATABASEMANAGER_H
