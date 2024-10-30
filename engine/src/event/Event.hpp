0d6a8e9720526b4f45e5623cd8fbceff3d6bdd32
ErnestGeorgeMaxim <ernest.maxim@student.unitbv.ro>
2024-10-30 14:37:57 +0200

    Added EventListener class for Event.hpp

--- engine/src/event/Event.hpp
+++ engine/src/event/Event.hpp
@@ -1,5 +1,33 @@
 #pragma once
+#include "Atlas.hpp"
 
-class Event {
 
+// Event callback function type
+using EventCallbackFn = std::function<void(Event&)>;
+
+// Event listener class
+class EventListener {
+public:
+    void subscribe(EventType type, const EventCallbackFn& callback) {
+        m_Callbacks[type].push_back(callback);
+    }
+
+    void unsubscribe(EventType type) {
+        m_Callbacks.erase(type);
+    }
+
+    void dispatchEvent(Event& event) {
+        auto type = event.getType();
+        if (m_Callbacks.find(type) != m_Callbacks.end()) {
+            for (auto& callback : m_Callbacks[type]) {
+                callback(event);
+                if (event.handled)
+                    break;
+            }
+        }
+    }
+
+private:
+    std::unordered_map<EventType, std::vector<EventCallbackFn>> m_Callbacks;
 };
+
