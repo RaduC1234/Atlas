8423a97c43ca3326366e6339ef8bb9a8575158b2
karinaconstandache <karinaconstandache12@gmail.com>
2024-10-30 14:45:57 +0200

    Added Event and EventDispatcher classes to Event.hpp

--- engine/src/event/Event.hpp
+++ engine/src/event/Event.hpp
@@ -1,6 +1,20 @@
 #pragma once
 #include "Atlas.hpp"
 
+// Event types enum
+enum class EventType {
+    NONE = 0,
+    KEY_PRESSED, KEY_RELEASED,
+    MOUSE_BUTTON_PRESSED, MOUSE_BUTTON_RELEASED, MOUSE_MOVED, MOUSE_SCROLLED
+};
+
+// Base event class
+class Event {
+public:
+    virtual ~Event() = default;
+    virtual EventType getType() const = 0;
+    bool handled = false;
+};
 
 // Event callback function type
 using EventCallbackFn = std::function<void(Event&)>;
@@ -29,5 +43,29 @@ public:
 
 private:
     std::unordered_map<EventType, std::vector<EventCallbackFn>> m_Callbacks;
+};
+
+// Event dispatcher helper class
+class EventDispatcher {
+public:
+    EventDispatcher(Event& event)
+        : m_Event(event) {}
+
+    template<typename T, typename F>
+    bool dispatch(const F& func) {
+        if (m_Event.getType() == T::getStaticType()) {
+            m_Event.handled |= func(static_cast<T&>(m_Event));
+            return true;
+        }
+        return false;
+    }
+
+private:
+    Event& m_Event;
+};
+
+class DefaultEvent : public Event {
+public:
+
 };
 
