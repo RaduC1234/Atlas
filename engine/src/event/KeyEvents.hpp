3525c6266557bcc5c0170718a3dda2843e67829c
ErnestGeorgeMaxim <ernest.maxim@student.unitbv.ro>
2024-11-17 18:28:28 +0200

    Added new sprites and updated keyEvent class

--- engine/src/event/KeyEvents.hpp
+++ engine/src/event/KeyEvents.hpp
@@ -1,24 +1,37 @@
 #pragma once
 #include "event/Event.hpp"
+#include "Keyboard.hpp"
 
 class KeyEvent : public Event {
 public:
-    KeyEvent(Key::KeyCode keycode) : m_KeyCode(keycode) {}
+    KeyEvent(KeyCode keycode)
+        : m_KeyCode(keycode) {}
 
-    Key::KeyCode getKeyCode() const { return m_KeyCode; }
+    KeyCode getKeyCode() const { return m_KeyCode; }
+
+    // Implement getName from base class
+    virtual const char* getName() const override { return "KeyEvent"; }
 
 protected:
-    Key::KeyCode m_KeyCode;
+    KeyCode m_KeyCode;
 };
 
 class KeyPressedEvent : public KeyEvent {
 public:
-    KeyPressedEvent(Key::KeyCode keycode, bool isRepeat = false)
+    KeyPressedEvent(KeyCode keycode, bool isRepeat = false)
         : KeyEvent(keycode), m_IsRepeat(isRepeat) {}
 
     bool isRepeat() const { return m_IsRepeat; }
 
-    virtual EventType getType() const override { return EventType::KEY_PRESSED; }
+    static EventType getStaticType() { return EventType::KEY_PRESSED; }
+    virtual EventType getType() const override { return getStaticType(); }
+    virtual const char* getName() const override { return "KeyPressedEvent"; }
+
+    virtual std::string toString() const override {
+        std::stringstream ss;
+        ss << "KeyPressedEvent: " << m_KeyCode << " (repeat = " << m_IsRepeat << ")";
+        return ss.str();
+    }
 
 private:
     bool m_IsRepeat;
@@ -26,8 +39,16 @@ private:
 
 class KeyReleasedEvent : public KeyEvent {
 public:
-    KeyReleasedEvent(Key::KeyCode keycode)
+    KeyReleasedEvent(KeyCode keycode)
         : KeyEvent(keycode) {}
 
-    virtual EventType getType() const override { return EventType::KEY_RELEASED; }
+    static EventType getStaticType() { return EventType::KEY_RELEASED; }
+    virtual EventType getType() const override { return getStaticType(); }
+    virtual const char* getName() const override { return "KeyReleasedEvent"; }
+
+    virtual std::string toString() const override {
+        std::stringstream ss;
+        ss << "KeyReleasedEvent: " << m_KeyCode;
+        return ss.str();
+    }
 };
