f7b7769bd49c901f0af4b5dfee599d36411e96b6
karinaconstandache <karinaconstandache12@gmail.com>
2024-12-06 22:26:01 +0200

    Improved DatabaseManager.hpp

--- server/src/data/DatabaseManager.hpp
+++ server/src/data/DatabaseManager.hpp
@@ -1,14 +1,12 @@
 #pragma once
 
-#include <Atlas.hpp>
-
 #include <sqlite_orm/sqlite_orm.h>
 #include "Player.hpp"
 #include "Match.hpp"
 
 namespace sql= sqlite_orm ;
 
-inline auto CreateStorage(const std::string &dbFile) {
+inline auto createStorage(const std::string &dbFile) {
     return sql::make_storage(
         dbFile,
         sql::make_table(
@@ -28,17 +26,20 @@ inline auto CreateStorage(const std::string &dbFile) {
             sql::make_column("id", &Match::getId, &Match::setId, sql::primary_key().autoincrement()),
             sql::make_column("timestamp", &Match::getTimestamp, &Match::setTimestamp),
             sql::make_column("glickoMatchWeight", &Match::getGlickoMatchWeight, &Match::setGlickoMatchWeight),
-            sql::make_column("winnerId", &Match::getWinnerId, &Match::setWinnerId) // Winner's Player ID
+            sql::make_column("winnerId", &Match::getWinnerId, &Match::setWinnerId)
         )
     );
 }
 
-using Storage = decltype(CreateStorage(""));
+using Storage = decltype(createStorage(""));
+
+template <typename T>
+concept DatabaseType = std::same_as<T, Player> || std::same_as<T, Match>;
 
 class DatabaseManager {
 public:
     static void init(const std::string &dbFile) {
-        m_storage = new Storage(CreateStorage(dbFile));
+        m_storage = new Storage(createStorage(dbFile));
         m_storage->sync_schema();
     }
 
@@ -46,22 +47,29 @@ public:
         delete m_storage;
     }
 
-    template<typename T>
+    template <DatabaseType T>
     static void create(const T &entity) {
         m_storage->insert(entity);
     }
 
-    template<typename T>
+    template <DatabaseType T, typename... Args>
+    static T emplace_create(Args&&... args) {
+        T entity(std::forward<Args>(args)...);
+        m_storage->insert(entity);
+        return entity;
+    }
+
+    template <DatabaseType T>
     static std::vector<T> getAll() {
         return m_storage->get_all<T>();
     }
 
-    template<typename T>
+    template <DatabaseType T>
     static void update(const T &entity) {
         m_storage->update(entity);
     }
 
-    template<typename T>
+    template <DatabaseType T>
     static void remove(const T &entity) {
         m_storage->remove<T>(entity.getId());
     }
